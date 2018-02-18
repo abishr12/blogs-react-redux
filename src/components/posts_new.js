1df223a68d3c390d79c4fd0c48ddb31f9efc2828
abishr <adhambishr@gmail.com>
2018-02-17 17:13:54 -0800

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">{touched ? error : ""}</div>
       </div>
     );
   }
 
+  onSubmit(values) {}
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderField} />
         <Field
-          label="Categores"
+          label="Categories"
           name="categories"
           component={this.renderField}
         />
@@ -25,6 +32,12 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
@@ -40,16 +53,16 @@ function validate(values) {
   //   errors.title = "Title must be at least 3 characters";
   // }
 
-  if (!values.titile) {
-    errors.title = "Enter a title ";
+  if (!values.title) {
+    errors.title = "Enter a title please";
   }
 
   if (!values.categories) {
-    errors.categories = "Enter some categories";
+    errors.categories = "Enter some categories please";
   }
 
-  if (!values.categories) {
-    errors.categories = "Enter some content please";
+  if (!values.content) {
+    errors.content = "Enter some content please";
   }
   return errors;
 }
